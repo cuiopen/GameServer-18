f79932ee76e96ba8d1deb47eeb6896526b3a3578
eiaeii <765865368@qq.com>
2017-01-18 18:47:09 +0800

    lua 例子

--- GameServer/Scripts/hello.lua
+++ GameServer/Scripts/hello.lua
@@ -1,1 +1,13 @@
-print("Hello World!");
+print("hello.lua have been load.");
+
+function showinfo()
+	print("welcome to lua world.");
+end
+
+function showstr(str)
+	print("the string you input is:" .. str);
+end
+
+function add(x, y)
+	return x + y;
+end
--- GameServer/main.cpp
+++ GameServer/main.cpp
@@ -12,9 +12,35 @@ extern "C"
 int main(int argc, char *argv[])
 {
 	lua_State *pLua = nullptr;
+
+	//初始化lua
 	pLua = luaL_newstate();
+
+	//载入lua基本库
 	luaL_openlibs(pLua);
+
+	//运行脚本文件
 	luaL_dofile(pLua, "./Scripts/hello.lua");
+
+	//获取lua中的showinfo函数
+	lua_getglobal(pLua, "showinfo");
+
+	//调用无参lua函数，无返回值
+	lua_pcall(pLua, 0, 0, 0);
+
+	const char *pstr = "I'm from c++!";
+	lua_getglobal(pLua, "showstr");
+	lua_pushstring(pLua, pstr);
+	
+	//调用一个参数的lua函数，无返回值
+	lua_pcall(pLua, 1, 0, 0);
+
+	lua_getglobal(pLua, "add");
+	lua_pushinteger(pLua, 2);
+	lua_pushinteger(pLua, 3);
+	lua_pcall(pLua, 2, 1, 0);
+	printf("lua add function return val is %lld\n", lua_tointeger(pLua, -1));
+
 	lua_close(pLua);
 	return 0;
 }
